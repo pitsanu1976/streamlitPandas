ab9ce2134c0282cce8be5d21051be0ae8d2bf542
pitsanu1976 <pitsanu.1976@gmail.com>
2023-06-15 22:00:38 +0700

    commit initial

---
+++ main.py
@@ -0,0 +1,31 @@
+from dotenv import load_dotenv
+import os
+import streamlit as st
+import pandas as pd
+from pandasai import PandasAI
+from pandasai.llm.openai import OpenAI
+
+load_dotenv()
+
+API_KEY = os.environ['openai_api_key']
+
+llm = OpenAI(api_token=API_KEY)
+pandas_ai = PandasAI(llm)
+
+st.title("Prompt-driven analysis with PandasAI")
+
+uploaded_file = st.file_uploader("Upload a csv file for analysis", type=['csv'])
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    st.write(df.head(3))
+    
+    prompt = st.text_area("Enter your prompt")
+    
+    if st.button("Generate"):
+        if prompt:
+            with st.spinner("Generate response..."):
+                st.write(pandas_ai.run(df,prompt=prompt))
+        else:
+            st.warning("Please enter a prompt")
+        
+
